044b13eea2fdca3e66ff72a2a7bef7f755b3b5bc
liompei <blm1137694912@gmail.com>
2016-10-11 15:31:38 +0800

    添加LeanCloud组件,完善注册布局

--- app/src/main/java/com/liompei/zhihudemo/ui/fragment/RegisterFinishFragment.java
+++ app/src/main/java/com/liompei/zhihudemo/ui/fragment/RegisterFinishFragment.java
@@ -1,5 +1,10 @@
 package com.liompei.zhihudemo.ui.fragment;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.liompei.zhihudemo.MainActivity;
 import com.liompei.zhihudemo.R;
 import com.liompei.zhihudemo.base.BaseFragment;
 
@@ -8,7 +13,11 @@ import com.liompei.zhihudemo.base.BaseFragment;
  * on 2016/10/11.
  */
 
-public class RegisterFinishFragment extends BaseFragment {
+public class RegisterFinishFragment extends BaseFragment implements View.OnClickListener {
+
+
+    private RelativeLayout to_start;  //开始吧
+
     @Override
     protected int getLayout() {
         return R.layout.fragment_register_finish;
@@ -17,5 +26,18 @@ public class RegisterFinishFragment extends BaseFragment {
     @Override
     protected void initView() {
 
+        to_start = (RelativeLayout) findViewById(R.id.to_start);
+        to_start.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.to_start:
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), MainActivity.class);
+                break;
+        }
     }
 }
